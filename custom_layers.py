4de5c5843140cab0dabdfc6e4f2e78167cc0f812
AshleySpindler <a.spindler@herts.ac.uk>
2023-09-19 11:42:13 +0100

    Create custom_layers.py
    take the custom layer blocks out of the remain model file

---
+++ custom_layers.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+
+# Upsampling Block for the Decoder
+class upsample(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super().__init__(**kwargs)
+        self.conv = nn.ConvTranspose2d(in_channels, out_channels, 3,
+                                stride=2, padding='same')
+
+    def forward(self, x):
+        return self.conv(x)
+
+# Conv Block for ResNet
+class ConvBlock(nn.Module):
+    def __init__(self, in_chnnels, out_chnnels, **kwargs):
+        super().__init__(**kwargs)
+        self.conv = nn.Conv2d(in_channels, out_channels, 3,
+                              stride=1, padding='same')
+        self.batchnorm = nn.BatchNorm2d(out_channels)
+
+        self.block = nn.Sequential(self.conv,
+                                   self.batchnorm)
+
+    def forward(self, x):
+
+        return self.block(x)
+
+# Resnet Block
+class resnet(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super().__init__(**kwargs)
+        self.block1 = ConvBlock(in_channels, out_chanells)
+        self.act1 = nn.Mish()
+        self.block2 = ConvBlock(out_channels, out_channels)
+        self.res_conv = nn.Conv2d(out_channels, out_channels, 1, padding='same') if in_channels != out_channels else nn.Identity()
+        self.act2 = nn.Mish()
+
+    def forward(self, x):
+        h = self.block1(x)
+        h = self.act1(h)
+        h = self.block2(h)
+        return self.act2(h + self.res_conv(x))
