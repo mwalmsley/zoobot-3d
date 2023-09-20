b283c96dcdf9dd8f76d39a117ba4f6975a09938d
Mike Walmsley <walmsleymk1@gmail.com>
2023-09-20 17:19:23 -0400

    start integrating A's model with M's dataloading

--- gz3d_pytorch_model.py
+++ gz3d_pytorch_model.py
@@ -1,92 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import logging
-from functools import partial
 
 import torch
-from torch import nn, Tensor
+from torch import nn
 from torch.nn import functional as F
 
-import pytorch_lightning as pl
-from torchmetrics import Accuracy
+# import pytorch_lightning as pl
+# from torchmetrics import Accuracy
 
-import pytorch_losses as losses
+# re-using from Zoobot
+from zoobot.pytorch.training import losses
+from zoobot.pytorch.estimators import define_model, efficientnet_custom
+from zoobot.pytorch.estimators.custom_layers import PermaDropout
+
+# new custom layers
 from custom_layers import DownSample, UpSample, ConvBlock, ResNet
-import efficientnet_custom
 
-class GenericLightningModule(pl.LightningModule):
-    """
-    All Zoobot models use the lightningmodule API and so share this structure
-    super generic, just to outline the structure. nothing specific to dirichlet, gz, etc
-    only assumes an encoder and a head
-    """
-
-    def __init__(
-        self,
-        *args,  # to be saved as hparams
-        ):
-        super().__init__()
-        self.save_hyperparameters()  # saves all args by default
-        self.setup_metrics()
-
-
-    def setup_metrics(self):
-        # these are ignored unless output dim = 2
-        self.train_accuracy = Accuracy(task='binary')
-        self.val_accuracy = Accuracy(task='binary')
-        # self.log_on_step = False
-        # self.log_on_step is useful for debugging, but slower - best when log_every_n_steps is fairly large
-
-
-    def forward(self, x):
-        x = self.encoder(x)
-        return self.head(x)
-    
-    def make_step(self, batch, batch_idx, step_name):
-        x, labels = batch
-        predictions = self(x)  # by default, these are Dirichlet concentrations
-        loss = self.calculate_and_log_loss(predictions, labels, step_name)      
-        return {'loss': loss, 'predictions': predictions, 'labels': labels}
-
-    def calculate_and_log_loss(self, predictions, labels, step_name):
-        raise NotImplementedError('Must be subclassed')
-
-    def configure_optimizers(self):
-        raise NotImplementedError('Must be subclassed')
-
-    def training_step(self, batch, batch_idx):
-        return self.make_step(batch, batch_idx, step_name='train')
-
-    def on_training_batch_end(self, outputs, *args):
-        self.log_outputs(outputs, step_name='train')
-
-    def validation_step(self, batch, batch_idx):
-        return self.make_step(batch, batch_idx, step_name='validation')
-
-    def on_validation_batch_end(self, outputs, *args):
-        self.log_outputs(outputs, step_name='validation')
-
-    def test_step(self, batch, batch_idx):
-        return self.make_step(batch, batch_idx, step_name='test')
-
-    def on_test_batch_end(self, outputs, *args):
-         self.log_outputs(outputs, step_name='test')
-
-    
-    def predict_step(self, batch, batch_idx, dataloader_idx=0):
-        # https://pytorch-lightning.readthedocs.io/en/stable/common/lightning_module.html#inference
-        # this calls forward, while avoiding the need for e.g. model.eval(), torch.no_grad()
-        # x, y = batch  # would be usual format, but here, batch does not include labels
-        return self(batch)
-
-class ZooBot3D(GenericLightningModule):
+class ZooBot3D(define_model.GenericLightningModule):
     def __init__(self,
                  output_dim = 34,
                  input_size = 128,
                  n_channels=3,
                  n_filters=32,
                  dim_mults=(1, 2, 4, 8),
-                 n_classes=4,
+                 n_classes=4,  # sets output dim 1 
                  drop_rates=(0,0,0.3,0.3),
                  test_time_dropout=False,
                  head_dropout=0.5,
@@ -110,8 +48,9 @@ class ZooBot3D(GenericLightningModule):
                                                test_time_dropout,
                                                head_dropout)
         # Losses - Set both separately then just add together? MSE for real seg maps, BXE for threshold
-        # Not really sure how Torch parses the loss functions yet, need to handle the multiple outputs
-        self.dirichlet_loss = get_dirichlet_loss_func(question_index_groups)
+        # TODO Not really sure how Torch parses the loss functions yet, need to handle the multiple outputs
+        # Mike: as long as make step returns a dict with 'loss' key, it should work automatically
+        self.dirichlet_loss = define_model.get_dirichlet_loss_func(question_index_groups)
         self.seg_loss = F.mse_loss
 
         # build decoder
@@ -131,19 +70,28 @@ class ZooBot3D(GenericLightningModule):
     def make_step(self, batch, batch_idx, step_name):
         ### This is going to override the generic make step, needs to get predictions for both labels and seg maps
         ### Possibly where we skip sep maps for images that don't need them?
-        x, (labels, seg_maps) = batch
-        pred_labels, pred_maps = self(x)
-        loss = self.calculate_and_log_loss((pred_labels, pred_maps), (labels, seg_maps), step_name)      
-        return {'loss': loss, 'predictions': pred_labels, 'labels': labels, 'predicted_maps': pred_maps, 'seg_maps': seg_maps}
+        # x, (labels, seg_maps) = batch
 
-    def calculate_and_log_loss(self, predictions, labels_maps, step_name):
+
+        pred_labels, pred_maps = self(batch['image'])
+        loss = self.calculate_and_log_loss((pred_labels, pred_maps), batch, step_name)      
+        return {
+            'loss': loss,
+            'predictions': pred_labels,
+            'labels': batch['label_cols'],
+            'predicted_maps': pred_maps,
+            'seg_maps': seg_maps
+        }
+
+    def calculate_and_log_loss(self, predictions, batch, step_name):
         # loss logging, found it!
         pred_labels, pred_maps = predictions
-        labels, seg_maps = labels_maps
         # self.loss_func returns shape of (galaxy, question), mean to ()
-        multiq_loss = self.dirichlet_loss(pred_labels, labels, sum_over_questions=False)
+        multiq_loss = self.dirichlet_loss(pred_labels, batch['label_cols'], sum_over_questions=False)
+
+        seg_maps = torch.stack([batch['spiral_mask'], batch['bar_mask']])  # TODO dims probably wrong
         seg_loss = self.seg_loss(seg_maps, pred_maps, reduction='none') # Reduction here?
-        # if hasattr(self, 'schema'):
+
         self.log_loss_per_question(multiq_loss, prefix=step_name)
         self.log_loss_per_seg_map(seg_loss, prefix=step_name)
         # sum over questions and take a per-device mean
@@ -240,66 +188,15 @@ def get_pytorch_dirichlet_head(encoder_dim: int, output_dim: int, test_time_drop
     modules_to_use.append(nn.Flatten(start_dim=1))
     if test_time_dropout:
         logging.info('Using test-time dropout')
-        dropout_layer = custom_layers.PermaDropout
+        dropout_layer = PermaDropout
     else:
         logging.info('Not using test-time dropout')
         dropout_layer = torch.nn.Dropout
     modules_to_use.append(dropout_layer(dropout_rate))
     # TODO could optionally add a bottleneck layer here
-    modules_to_use.append(custom_top_dirichlet(encoder_dim, output_dim))
+    modules_to_use.append(efficientnet_custom.custom_top_dirichlet(encoder_dim, output_dim))
 
     return nn.Sequential(*modules_to_use)
-
-def custom_top_dirichlet(input_dim, output_dim):
-    """
-    Final dense layer used in GZ DECaLS (after global pooling). 
-    ``output_dim`` neurons with an activation of ``tf.nn.sigmoid(x) * 100. + 1.``, chosen to ensure 1-100 output range
-    This range is suitable for parameters of Dirichlet distribution.
-
-    Args:
-        output_dim (int): Dimension of dense layer e.g. 34 for decision tree with 34 answers
-
-    Returns:
-        nn.Sequential: nn.Linear followed by 1-101 sigmoid activation
-    """
-    return nn.Sequential(
-        # LinearWithCustomInit(in_features=input_dim, out_features=output_dim),
-        nn.Linear(in_features=input_dim, out_features=output_dim),
-        ScaledSigmoid()
-    )
-
-class ScaledSigmoid(nn.modules.Sigmoid):
-    # https://pytorch.org/docs/stable/_modules/torch/nn/modules/activation.html#ReLU
-
-    def forward(self, input: Tensor) -> Tensor:
-        """
-        Args:
-            input (Tensor): any vector. typically logits from a neural network
-
-        Returns:
-            Tensor: input mapped to range (1, 101) via torch.sigmoid
-        """
-        return torch.sigmoid(input) * 100. + 1.  # could make args if I needed
-
-
-def get_dirichlet_loss_func(question_index_groups):
-    # This just adds schema.question_index_groups as an arg to the usual (labels, preds) loss arg format
-    # Would use lambda but multi-gpu doesn't support as lambda can't be pickled
-    return partial(dirichlet_loss, question_index_groups=question_index_groups)
-
-
-    # accept (labels, preds), return losses of shape (batch, question)
-def dirichlet_loss(preds, labels, question_index_groups, sum_over_questions=False):
-    # pytorch convention is preds, labels for loss func
-    # my and sklearn convention is labels, preds for loss func
-
-    # multiquestion_loss returns loss of shape (batch, question)
-    # torch.sum(multiquestion_loss, axis=1) gives loss of shape (batch). Equiv. to non-log product of question likelihoods.
-    multiq_loss = losses.calculate_multiquestion_loss(labels, preds, question_index_groups)
-    if sum_over_questions:
-        return torch.sum(multiq_loss, axis=1)
-    else:
-        return multiq_loss
 
 
 # decoder clss, include the skip connections — how to save on GPU cycles by not forward passing unecessarily?
@@ -344,34 +241,16 @@ class pytorch_decoder_module(nn.Module):
         return self.final_conv(x)
 
 
+if __name__ == '__main__':
 
+    model = ZooBot3D()
+    encoder = model.encoder
+    decoder = model.decoder
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # test with dummy inputs
+    image = torch.rand((1, 3, 128, 128))
+    encoded_image, skip_outputs = encoder(image)
+    # print(image.shape)
+    print(encoded_image.shape)
+    decoded_output = decoder((encoded_image, skip_outputs))
+    print(decoded_output.shape)
---
+++ train.py
@@ -0,0 +1,84 @@
+import typing
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import pytorch_lightning as pl
+
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+from galaxy_datasets import transforms
+
+import gz3d_pytorch_model, pytorch_datamodule
+
+# lazy copy of the below, but with additional_targets=
+# https://github.com/mwalmsley/galaxy-datasets/blob/main/galaxy_datasets/transforms.py#L6
+def default_segmentation_transforms(
+    crop_scale_bounds=(0.7, 0.8),
+    crop_ratio_bounds=(0.9, 1.1),
+    resize_after_crop=224, 
+    pytorch_greyscale=False
+    ) -> typing.Dict[str, typing.Any]:
+
+    transforms_to_apply = [
+        A.Lambda(name="RemoveAlpha", image=transforms.RemoveAlpha(), always_apply=True)
+    ]
+
+    if pytorch_greyscale:
+        transforms_to_apply += [
+            A.Lambda(
+                name="ToGray", image=transforms.ToGray(reduce_channels=True), always_apply=True
+            )
+        ]
+
+    transforms_to_apply += [
+        A.Rotate(limit=180, interpolation=1,
+                    always_apply=True, border_mode=0, value=0),
+        A.RandomResizedCrop(
+            height=resize_after_crop,  # after crop resize
+            width=resize_after_crop,
+            scale=crop_scale_bounds,  # crop factor
+            ratio=crop_ratio_bounds,  # crop aspect ratio
+            interpolation=1,
+            always_apply=True
+        ),  # new aspect ratio
+        A.VerticalFlip(p=0.5),
+    ]
+
+    return A.Compose(
+        transforms_to_apply,
+        additional_targets={'spiral_mask': 'image', 'bar_mask': 'image'}
+    )
+
+
+def main():
+
+    df = pd.read_csv('/Users/user/repos/zoobot-3d/data/gz3d_and_gz_desi_master_catalog.csv')
+    df = df[df['smooth-or-featured_featured-or-disk_fraction'] > 0.5]
+    df = df[df['disk-edge-on_yes_fraction'] < 0.5]
+    df = df[df['has-spiral-arms_yes_fraction'] > 0.5]
+    print(len(df))
+
+    image_size = 128
+    model = gz3d_pytorch_model.ZooBot3D(input_size=image_size)
+
+    datamodule = pytorch_datamodule.SegmentationDataModule(
+        train_catalog=df[:2],
+        val_catalog=df[2:4],
+        test_catalog=df[4:6],
+        batch_size=2,
+        num_workers=1,
+        transform=default_segmentation_transforms()
+    )
+    datamodule.setup('fit')
+
+    trainer = pl.Trainer(
+        accelerator='auto',
+        max_epochs=2
+    )
+
+    trainer.fit(model, datamodule)
+
+if __name__ == '__main__':
+
+    main()
