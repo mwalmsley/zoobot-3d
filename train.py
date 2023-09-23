af5b6e499539ad0dcde20a3360e2bcb56da9ac35
Mike Walmsley <walmsleymk1@gmail.com>
2023-09-22 20:29:08 -0400

    typo

--- train.py
+++ train.py
@@ -211,7 +211,7 @@ def main():
         
         strategy_obj = DDPStrategy(
             accelerator=wandb_config.accelerator,
-            parallel_devices=[torch.device(f"cuda:{i}") for i in wandb_config.devices],
+            parallel_devices=[torch.device(f"cuda:{i}") for i in range(wandb_config.devices)],
             find_unused_parameters=False
         )
     else:
