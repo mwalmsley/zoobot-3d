3fa0f59947790dd09fea935d8cbd0ad202d3efa8
Mike Walmsley <walmsleymk1@gmail.com>
2023-09-26 19:05:02 -0400

    check I didn't break smth

--- run_experiments.sh
+++ run_experiments.sh
@@ -11,8 +11,8 @@
 # sbatch train.sh debug=True
 
 # seg loss only, gz3d only (as even simpler baseline)
-# sbatch train.sh "oversampling_ratio=1 use_vote_loss=False use_seg_loss=True vote_loss_weighting=0.01 gz3d_galaxies_only=True"
-# sbatch train.sh "oversampling_ratio=1 use_vote_loss=True use_seg_loss=True vote_loss_weighting=0.01 gz3d_galaxies_only=True" 
+sbatch train.sh "oversampling_ratio=1 use_vote_loss=False use_seg_loss=True vote_loss_weighting=0.01 gz3d_galaxies_only=True"
+sbatch train.sh "oversampling_ratio=1 use_vote_loss=True use_seg_loss=True vote_loss_weighting=0.01 gz3d_galaxies_only=True" 
 # repeat with L1 loss instead
 # sbatch train.sh "oversampling_ratio=1 use_vote_loss=False use_seg_loss=True seg_loss_weighting=100 gz3d_galaxies_only=True seg_loss_metric=l1"
 # sbatch train.sh "oversampling_ratio=1 use_vote_loss=True use_seg_loss=True seg_loss_weighting=100 gz3d_galaxies_only=True seg_loss_metric=l1" 
@@ -21,8 +21,8 @@
 # set L1 and constnorm as default
 
 
-sbatch train.sh "oversampling_ratio=5 use_vote_loss=False use_seg_loss=True vote_loss_weighting=0.01 max_additional_galaxies=10000"
-sbatch train.sh "oversampling_ratio=5 use_vote_loss=True use_seg_loss=True vote_loss_weighting=0.01 max_additional_galaxies=10000"
+# sbatch train.sh "oversampling_ratio=5 use_vote_loss=False use_seg_loss=True vote_loss_weighting=0.01 max_additional_galaxies=10000"
+# sbatch train.sh "oversampling_ratio=5 use_vote_loss=True use_seg_loss=True vote_loss_weighting=0.01 max_additional_galaxies=10000"
 
 
 # all galaxies, checking each loss
