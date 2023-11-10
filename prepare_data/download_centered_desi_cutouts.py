4af4540478224c1ac43c29607d41df9cb72ba4bd
Mike Walmsley <walmsleymk1@gmail.com>
2023-11-10 14:17:34 -0500

    get sami images

--- prepare_data/download_centered_desi_cutouts.py
+++ prepare_data/download_centered_desi_cutouts.py
@@ -9,6 +9,9 @@ import pandas as pd
 
 if __name__ == '__main__':
 
+    # target_list_survey = 'manga'
+    target_list_survey = 'sami'
+
     if os.path.isdir('/share/nas2'):
         galahad = True
         repo_dir = '/share/nas2/walml/repos/zoobot-3d'
@@ -18,27 +21,27 @@ if __name__ == '__main__':
         sys.path.append('/share/nas2/walml/repos/download_DECaLS_images')
     else:
         galahad = False
-        repo_dir = '/Users/user/repos/zoobot-3d'
+        repo_dir = '/home/walml/repos/zoobot-3d'
         data_dir = os.path.join(repo_dir, 'data')
         # base_image_dir = os.path.join(data_dir, 'desi')
-        base_image_dir = '/Volumes/beta/galaxy_zoo/segmentation/data/desi'
-        sys.path.append('/Users/user/repos/download_DECaLS_images')
+        # base_image_dir = '/Volumes/beta/galaxy_zoo/segmentation/data/desi'
+        base_image_dir = os.path.join(data_dir, target_list_survey)      
+        sys.path.append('/home/walml/repos/download_DECaLS_images')
 
     import downloader  # part of the private download_DECaLS_images repo above
     
 
     df = pd.read_csv(
-        os.path.join(
-            data_dir, 'gz3d_and_gz_desi_matches.csv')
+        os.path.join(data_dir, f'{target_list_survey}_and_gz_desi_matches.csv')
     )
 
-    if not galahad:
-        df = df[:100]
+    # if not galahad:
+    #     df = df[:100]
     
     for _, galaxy in tqdm(list(df.iterrows())):
         # center on MANGA source, not DESI source
-        galaxy['ra'] = galaxy['ra_manga']
-        galaxy['dec'] = galaxy['dec_manga']
+        galaxy['ra'] = galaxy[f'ra_{target_list_survey}']
+        galaxy['dec'] = galaxy[f'dec_{target_list_survey}']
         rgb_format = 'jpg'
 
         fits_dir = os.path.join(base_image_dir, 'fits')
